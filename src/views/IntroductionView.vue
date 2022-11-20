aee454b96e5771b285ba295c6fd6ac507ed0e009
Ruslan <seitensogekihei@protonmail.ch>
2022-11-20 21:39:09 +0300

    feat: intro view

---
+++ src/views/IntroductionView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const kanjiButtonTitle = 'Kanji';
+const vocabButtonTitle = 'Vocabulary';
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to JLPT N3 Study Area!</h1>
+    <h3>Please choose what you want to master today:</h3>
+    <div class="buttons">
+        <router-link to="/kanji" v-slot="{ navigate }">
+            <button @click="navigate">
+                {{ kanjiButtonTitle }}
+            </button>
+        </router-link>
+        <router-link to="/vocabulary" v-slot="{ navigate }">
+            <button @click="navigate">
+                {{ vocabButtonTitle }}
+            </button>
+        </router-link>
+    </div>
+  </div>
+</template>
+
+<style scope>
+button {
+    width: 100px;
+    height: 50px;
+    border: 3px solid black;
+    border-radius: 20%;
+}
+
+.buttons {
+    display: flex;
+}
+</style>
